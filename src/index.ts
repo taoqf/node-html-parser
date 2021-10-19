2f04c976088384dd38d9512aa311805895e62e95
二刺螈 <i@songe.li>
2021-10-19 23:36:48 +0800

    fix: support esbuild

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 export { default as CommentNode } from './nodes/comment';
 export { default as HTMLElement, Options } from './nodes/html';
-export { default as parse, default } from './parse';
+export { default as parse } from './parse';
 export { default as valid } from './valid';
 export { default as Node } from './nodes/node';
 export { default as TextNode } from './nodes/text';
 export { default as NodeType } from './nodes/type';
+
+Object.assign(parse, {
+  CommentNode,
+  HTMLElement,
+  Options,
+  parse,
+  valid,
+  Node,
+  TextNode,
+  NodeType,
+});

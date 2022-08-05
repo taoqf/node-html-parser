9c1864f949961da276e8e3b1e83c6b644f6f8b54
谭杨 <tanyang@xindong.com>
2022-08-05 14:39:46 +0800

    fix: fix vite build error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-export { default as CommentNode } from './nodes/comment';
-export { default as HTMLElement, Options } from './nodes/html';
-export { default as parse, default } from './parse';
-export { default as valid } from './valid';
-export { default as Node } from './nodes/node';
-export { default as TextNode } from './nodes/text';
-export { default as NodeType } from './nodes/type';
+import { default as CommentNode } from './nodes/comment';
+import { default as HTMLElement } from './nodes/html';
+import type { Options } from './nodes/html';
+import { default as parse } from './parse';
+import { default as valid } from './valid';
+import { default as Node } from './nodes/node';
+import { default as TextNode } from './nodes/text';
+import { default as NodeType } from './nodes/type';
+
+export default {
+  CommentNode,
+  HTMLElement,
+  parse,
+  valid,
+  Node,
+  TextNode,
+  NodeType,
+};
+
+export {
+  CommentNode,
+  HTMLElement,
+  parse,
+  valid,
+  Node,
+  TextNode,
+  NodeType,
+};
+
+export type {
+  Options,
+};
+

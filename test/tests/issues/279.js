aa98fb21dae97470d61540e3dda9c390f8bc0aa0
taoqf <tao_qiufeng@126.com>
2024-12-26 09:35:31 +0800

    #279 add test

---
+++ test/tests/issues/279.js
@@ -0,0 +1,9 @@
+const { parse, valid } = require('@test/test-target');
+const fs = require('fs');
+
+describe('issue 279', function () {
+	it('HTML considered invalid wrongly', function () {
+		const content = fs.readFileSync(__dirname + '/../../assets/html/beego package - github.com_astaxie_beego - Go Packages.html', 'utf-8');
+		valid(content).should.equal(true);
+	});
+});

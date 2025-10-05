32136c792d7dc36e1bc4a57d73ff15c3494909d0
taoqf <tao_qiufeng@126.com>
2025-10-05 20:40:36 +0800

    refactor: remove Node, TextNode, NodeType, and related parsing logic
    - Deleted Node class and its methods, which served as a base for TextNode and HTMLElement.
    - Removed TextNode class, including its properties and methods for handling text content.
    - Eliminated NodeType enum that defined various node types.
    - Removed parse function export from parse.ts, along with the valid function that utilized it.
    - Deleted VoidTag class responsible for handling void elements in HTML.
    - Commented out tests related to node ranges due to the removal of node classes.
    - Updated tsconfig.json to include "pkg" in the include array.

--- src/back.ts
+++
@@ -1,3 +0,0 @@
-export default function arr_back<T>(arr: T[]) {
-	return arr[arr.length - 1];
-}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
-import CommentNode from './nodes/comment';
-import HTMLElement, { Options } from './nodes/html';
-import Node from './nodes/node';
-import TextNode from './nodes/text';
-import NodeType from './nodes/type';
-import baseParse from './parse';
-import valid from './valid';
+import { is_valid_html, JsCommentNode, JsElement, JsNode, JsNodeType, JsParseOptions, JsTextNode, parse_html_with_options } from '../pkg';
 
-export { Options } from './nodes/html';
-export {
-	parse,
-	HTMLElement,
-	CommentNode,
-	valid,
-	Node,
-	TextNode,
-	NodeType
-};
+// import CommentNode from './nodes/comment';
+// import Node from './nodes/node';
+// import TextNode from './nodes/text';
+// import NodeType from './nodes/type';
+// import valid from './valid';
 
-export default function parse(data: string, options = {} as Partial<Options>) {
-	return baseParse(data, options);
+export { JsParseOptions as Options, JsElement as HTMLElement, is_valid_html as valid, JsNode as Node, JsNodeType as NodeType, JsCommentNode as CommentNode, JsTextNode as TextNode, parse_html as parse, parse_html_with_options, version } from '../pkg';
+// export {
+// 	CommentNode,
+// 	valid,
+// 	Node,
+// 	TextNode,
+// 	NodeType
+// };
+
+export default function parse(data: string, options = {} as Partial<JsParseOptions>) {
+	const opts = new JsParseOptions();
+	if (options.lower_case_tag_name !== undefined) {
+		opts.lower_case_tag_name = options.lower_case_tag_name;
+	}
+	if (options.comment !== undefined) {
+		opts.comment = options.comment;
+	}
+	if (options.fix_nested_a_tags !== undefined) {
+		opts.fix_nested_a_tags = options.fix_nested_a_tags;
+	}
+	if (options.parse_none_closed_tags !== undefined) {
+		opts.parse_none_closed_tags = options.parse_none_closed_tags;
+	}
+	return parse_html_with_options(data, opts);
 }
 
-parse.parse = baseParse;
-parse.HTMLElement = HTMLElement;
-parse.CommentNode = CommentNode;
-parse.valid = valid;
-parse.Node = Node;
-parse.TextNode = TextNode;
-parse.NodeType = NodeType;
+parse.parse = parse;
+parse.HTMLElement = JsElement;
+parse.CommentNode = JsCommentNode;
+parse.valid = is_valid_html;
+parse.Node = JsNode;
+parse.TextNode = JsTextNode;
+parse.NodeType = JsNodeType;
